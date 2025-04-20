59ccc9ad905a4a394905b5ef032b0671e14834e6
kirikopen <penkiriko595@gmail.com>
2025-04-20 22:04:05 +0700

    editing contain file of authController.js

--- UTS/controllers/authController.js
+++ UTS/controllers/authController.js
@@ -7,19 +7,19 @@ exports.register = (req, res) => {
         return res.status(400).json({ message: 'Field tidak boleh kosong.' });
     }
 
-    const existing = users.find(user => user.email === email);
+    const existing = users.find(user => user.email.toLowerCase() === email.toLowerCase());
     if (existing) {
         return res.status(409).json({ message: 'Email sudah terdaftar.' });
     }
 
-    users.push({ email, password });
+    users.push({ email: email.toLowerCase(), password });
     return res.status(201).json({ message: 'Registrasi berhasil!' });
 };
 
 exports.login = (req, res) => {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
 
-    const user = users.find(u => u.email === username && u.password === password);
+    const user = users.find(u => u.email === email.toLowerCase() && u.password === password);
     if (user) {
         res.status(200).json({ message: 'Login berhasil' });
     } else {
