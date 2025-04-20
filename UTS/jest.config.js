ebdc32137df85569d31693273e822c5cb8cd9564
kirikopen <penkiriko595@gmail.com>
2025-04-20 23:11:05 +0700

    editing contain file of jestconfig.js

--- UTS/jest.config.js
+++ UTS/jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    testEnvironment: 'node'
-  };
+  preset: 'ts-jest',  // Menggunakan ts-jest untuk menangani TypeScript
+  testEnvironment: 'node',  // Menggunakan environment node.js untuk testing
+};
